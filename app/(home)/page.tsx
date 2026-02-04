9235fd30600338cc6db613b09260ef4b96cc9678
leandroscalercio <leandroscalercio@gmail.com>
2026-02-04 16:05:27 -0300

    feat: add ai report generation openai

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,17 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
-import TransactionsPieChart from "./_components/transactions-pie.chart";
+
 import { getDashboard } from "../_data/get-dashboard";
-import ExpensesPerCategory from "./_components/expense-per-category";
+
 import LastTransactions from "./_components/last-transactions";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+import AiReportButton from "./_components/ai-report-button";
+import TransactionsPieChart from "./_components/transactions-pie.chart";
+import ExpensesPerCategory from "./_components/expense-per-category";
 
 interface HomeProps {
   searchParams: {
@@ -21,42 +24,44 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   if (!userId) {
     redirect("/login");
   }
-  const monthIsInvalid = !month || isMatch(month, "MM") === false;
+  const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    const currentMonth = String(new Date().getMonth() + 1).padStart(2, "0");
-    redirect(`/?month=${currentMonth}`);
+    redirect(`?month=${new Date().getMonth() + 1}`);
   }
-
-  const dashboardData = await getDashboard(month);
+  const dashboard = await getDashboard(month);
   const userCanAddTransaction = await canUserAddTransaction();
-
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
-      <div className="flex flex-col space-y-6 overflow-hidden p-6">
-        <div className="flex items-center justify-between">
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
+        <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          <TimeSelect />
+          <div className="flex items-center gap-3">
+            <AiReportButton
+              month={month}
+              hasPremiumPlan={
+                user.publicMetadata.subscriptionPlan === "premium"
+              }
+            />
+            <TimeSelect />
+          </div>
         </div>
-        <div className="grid grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards
               month={month}
-              {...dashboardData}
+              {...dashboard}
               userCanAddTransaction={userCanAddTransaction}
             />
-            <div className="grid grid-cols-2 gap-6 overflow-hidden">
-              <TransactionsPieChart {...dashboardData} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
-                expensesPerCategory={dashboardData.totalExpensePerCategory}
+                expensesPerCategory={dashboard.totalExpensePerCategory}
               />
             </div>
           </div>
-          <div className="flex flex-col">
-            <LastTransactions
-              lastTransactions={dashboardData.lastTransactions}
-            />
-          </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
