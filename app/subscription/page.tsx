8b1ebdbf3d6a950f6934b2b90b2e88c5d02150b3
leandroscalercio <leandroscalercio@gmail.com>
2026-02-05 14:15:23 -0300

    fix: adjust subscription page responsive

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -15,14 +15,15 @@ const SubscriptionPage = async () => {
   const user = await clerkClient().users.getUser(userId);
   const currentMonthTransactions = await getCurrentMonthTransactions();
   const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
+
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 p-4 md:p-6">
         <h1 className="text-2xl font-bold">Assinatura</h1>
 
-        <div className="flex gap-6">
-          <Card className="w-[450px]">
+        <div className="mx-auto flex max-w-[980px] flex-col items-center gap-6 md:flex-row md:items-stretch md:justify-center">
+          <Card className="w-full max-w-[450px]">
             <CardHeader className="border-b border-solid py-8">
               <h2 className="text-center text-2xl font-semibold">
                 Plano Básico
@@ -47,7 +48,7 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
 
-          <Card className="w-[450px]">
+          <Card className="w-full max-w-[450px]">
             <CardHeader className="relative border-b border-solid py-8">
               {hasPremiumPlan && (
                 <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
